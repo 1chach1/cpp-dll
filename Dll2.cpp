cc0f268dac7222a0dd182bbc8d429f0bdcf33c2b
ChaCh <malinovskii2001@gmail.com>
2023-03-21 18:19:43 +0500

    Initial commit

---
+++ Dll2.cpp
@@ -0,0 +1,22 @@
+﻿#pragma hdrstop
+#pragma argsused
+#include "Dll2.h"
+#include <wchar.h>
+
+__declspec(dllexport) int group()
+{
+	return int(581220);
+}
+
+__declspec(dllexport) void name(wchar_t* ptr)
+{
+    const wchar_t* str = L"Малиновский Александр Алексеевич";
+    wcscpy(ptr, str);
+}
+
+
+extern "C" int _libmain(unsigned long reason)
+{
+	return 1;
+}
+
---
+++ Dll2.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+extern "C" __declspec(dllexport) __stdcall int group();
+extern "C" __declspec(dllexport) __stdcall void name(wchar_t* ptr);

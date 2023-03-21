5fbba548848bfa5b04c608acd70e9ffb03cefd43
ChaCh <malinovskii2001@gmail.com>
2023-03-21 18:31:57 +0500

    Added new function getnameEn

--- Dll2.cpp
+++ Dll2.cpp
@@ -14,6 +14,11 @@ __declspec(dllexport) void getname(wchar_t* ptr)
     wcscpy(ptr, str);
 }
 
+__declspec(dllexport) void getnameEn(wchar_t* ptr)
+{
+	const wchar_t* str = L"Malinovskiy Alexander Alexeevich";
+    wcscpy(ptr, str);
+}
 
 extern "C" int _libmain(unsigned long reason)
 {
--- Dll2.h
+++ Dll2.h
@@ -3,3 +3,4 @@
 
 extern "C" __declspec(dllexport) __stdcall int getgroup();
 extern "C" __declspec(dllexport) __stdcall void getname(wchar_t* ptr);
+extern "C" __declspec(dllexport) __stdcall void getnameEn(wchar_t* ptr);
